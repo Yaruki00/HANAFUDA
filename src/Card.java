301e86c2c7138a26abf20cacd6c6662e44efb8d0
Yuta Kawabe <yuyu3165@gmail>
2012-11-01 13:53:39 +0900

    set yaku and change name

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,6 @@
 public class Card {
 	private String name;
+	// gokou, hanami, tsukimi, inoshikacho, akatan, aotan, tan, tane, kasu
 	private int[] yaku;
 	public Card(String name, int[] yaku) {
 		this.name = name;
